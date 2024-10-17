49a02e683d957c9a675d83a623cc8cc8086f12ad
Clara Quintela <claraquintela@gmail.com>
2024-10-17 15:42:19 -0400

    Config js config and absolute imports

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,19 @@
 import styles from "./Menu.module.css";
 import MenuLink from "../MenuLink";
 import logo from "../../assets/imgs/logo_site.png";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
     <header>
       <div className={styles.container_header}>
-        <MenuLink to="http://claraquintela.com">
+        <Link to="http://claraquintela.com">
           <img
             src={logo}
             alt="Logo Clara Quintela"
             className={styles.logo_header}
           />
-        </MenuLink>
+        </Link>
 
         <nav className={styles.navegacao}>
           <MenuLink to="/">Home</MenuLink>
