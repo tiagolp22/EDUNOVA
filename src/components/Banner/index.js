1a28e86cf73bb3d24211a45acc240e379f398b73
Clara Quintela <claraquintela@gmail.com>
2024-10-18 17:19:11 -0400

    Ajust banner and button (not yet accomplished)

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,28 +1,30 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import banner from "assets/imgs/edunova_banner.jpg";
+import bannerEN from "assets/imgs/edunova_bannerEN.jpg";
+import bannerPT from "assets/imgs/edunova_bannerPT.jpg";
 
 export default function Banner({ t }) {
+  const [currentBanner, setCurrentBanner] = useState(bannerPT);
+
+  useEffect(() => {
+    const chosenLanguage = localStorage.getItem("chosenLanguage");
+    setCurrentBanner(chosenLanguage === "en" ? bannerEN : bannerPT);
+  }, []);
 
   return (
-    <div className="relative w-full h-[320px]" id="home">
-      <div className="absolute">
-        <img
-          src={banner}
-          alt="banner"
-          className="object-cover object-center w-full h-full"
-        />
-      </div>
-      <div className="absolute inset-9 flex flex-col md:flex-row items-center justify-between">
-        <div className="md:w-1/2 mb-4 md:mb-0">
-          <h1 className=" text-[--azul-medio] md:text-6xl leading-tight my-8">
-            {t("hero")}
-          </h1>
-
+    <div className="relative w-full h-[320px] sm:h-auto" id="home">
+      <img
+        src={currentBanner}
+        alt={t("bannerAltText", "Edunova banner")}
+        className="w-full h-full object-cover object-center"
+      />
+      <div className="absolute inset-x-14 top-24 sm:bottom-[60%] md:top-60 flex flex-col md:flex-row items-start justify-between">
+        <div className="md:w-1/2">
           <Link
             to="/about"
-            className="px-6 py-3 bg-[--azul-medio] text-white font-medium rounded-full hover:bg-[--amarelo]  transition duration-200"
+            className="inline-block px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 bg-[--azul-medio] text-white text-[2vw] sm:text-[1.8vw] md:text-[1.5vw] font-medium rounded-full hover:bg-[--amarelo] transition duration-200"
           >
-            Contact Us
+            {t("contactUs", "Contact Us")}
           </Link>
         </div>
       </div>
