379163856117429aa7d0b84e69f962148ed8941a
Clara Quintela <claraquintela@gmail.com>
2024-10-18 13:50:45 -0400

    Insert banner
    Color ajust

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,6 +1,30 @@
 import styles from "./Banner.module.css";
-import banner_VM from "../../assets/imgs/banner_VM.jpg";
+import banner from "assets/imgs/edunova_banner.jpg";
 
 export default function Banner() {
-  return <img src={banner_VM} alt="Banner do site Visible Mending" className={styles.hero_banner}/>;
+  return (
+    <div className="relative w-full h-[320px]" id="home">
+      <div className="absolute">
+        <img
+          src={banner}
+          alt="banner"
+          className="object-cover object-center w-full h-full"
+        />
+      </div>
+      <div className="absolute inset-9 flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2 mb-4 md:mb-0">
+          <h1 className="text-grey-700 font-medium text-[--azul-medio] text-4xl md:text-5xl leading-tight mb-8">
+            Seu aprendizado, no seu ritmo, em qualquer lugar.
+          </h1>
+
+          <a
+            href="#contactUs"
+            className="px-6 py-3 bg-[#c8a876] text-white font-medium rounded-full hover:bg-[#c09858]  transition duration-200"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import styles from "./Menu.module.css";
-import logo from "../../assets/imgs/logo_site.png";
-import { Link } from "react-router-dom";
+import logo from "assets/imgs/EduNova_logo.gif";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Menu() {
   return (
     <header>
       <div classNameName={styles.container_header}>
-        <Link to="http://claraquintela.com">
-          <img src={logo} alt="Logo Clara Quintela" width="200" height="200" />
-        </Link>
-
-        <nav className="flex flex-wrap items-center justify-between p-3 bg-[#e8e8e5]">
-          <div className="text-xl">Arte Têxtil</div>
+        <nav className="flex flex-wrap items-center justify-between p-3 bg-[--azul-escuro]">
+          <div className="text-xl">
+            <Link to="/">
+              <img src={logo} alt="Logo EduNova" width="200" height="200" />
+            </Link>
+          </div>
           <div className="flex md:hidden">
             <button id="hamburger">
               <img
@@ -28,61 +28,47 @@ export default function Menu() {
               />
             </button>
           </div>
-          <div className=" toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 md:border-none">
-            <a
-              href="#home"
+          <div className=" toggle hidden w-full md:w-auto md:flex text-[--amarelo] text-right text-bold mt-5 md:mt-0 md:border-none">
+            <NavLink
+              to="/"
               className="block md:inline-block hover:text-blue-500 px-3 py-3 md:border-none"
             >
               Home
-            </a>
-            <a
+            </NavLink>
+            <NavLink
               href="#services"
               className="block md:inline-block hover:text-blue-500 px-3 py-3 md:border-none"
             >
               Services
-            </a>
-            <a
+            </NavLink>
+            <NavLink
               href="#aboutus"
               className="block md:inline-block hover:text-blue-500 px-3 py-3 md:border-none"
             >
               About us
-            </a>
-            <a
+            </NavLink>
+            <NavLink
               href="#gallery"
               className="block md:inline-block hover:text-blue-500 px-3 py-3 md:border-none"
             >
               Gallery
-            </a>
-            <a
+            </NavLink>
+            <NavLink
               href="#contactUs"
               className="block md:inline-block hover:text-blue-500 px-3 py-3 md:border-none"
             >
               Visit Us
-            </a>
+            </NavLink>
           </div>
 
           <div className="toggle w-full text-end hidden md:flex md:w-auto px-2 py-2 md:rounded">
-            <a href="tel:+123">
+            <NavLink href="tel:+123">
               <div className="flex justify-end">
-                <div className="flex items-center h-10 w-30 rounded-md bg-[#c8a876] text-white font-medium p-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-                    />
-                  </svg>
-                  Call now
+                <div className="flex items-center h-10 w-30 rounded-md bg-[--amarelo] text-[--azul-escuro] font-medium p-2">
+                  Login / Sign up
                 </div>
               </div>
-            </a>
+            </NavLink>
           </div>
         </nav>
       </div>
