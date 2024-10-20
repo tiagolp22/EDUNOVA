013e4757392b66a54d1236899417e0c3f8ec0381
Clara Quintela <claraquintela@gmail.com>
2024-10-20 18:33:54 -0400

    Add  component Footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import Banner from "components/Banner";
 import Catalogue from "components/Catalogue";
+import ExtraInfo from "components/ExtraInfo/ExtraInfo";
+import Footer from "components/Footer/Footer";
+import IconHome from "components/IconsHome/IconHome";
 
 export default function Home({ t, language }) {
   return (
     <div>
       <Banner t={t} />
-      <Catalogue t={t} language={language}/>
+      <Catalogue t={t} language={language} />
+      <ExtraInfo t={t} />
+      <IconHome />
+      <Footer />
     </div>
   );
 }
