ba51beef997aae64f56d9dcec5aec4e49bd820c9
Clara Quintela <claraquintela@gmail.com>
2024-10-22 11:33:28 -0400

    Add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer>
-      <div className="bg-gray-800 py-4 text-gray-400">
+      <div className="bg-[--azul-escuro] py-4 text-gray-400">
         <div className="container px-4 mx-auto">
           <div className="-mx-4 flex flex-wrap justify-between">
             <div className="px-4 my-4 w-full xl:w-1/5">
-              <a href="/" className="block w-56 mb-10">
+              <Link to="/" className="block w-56 mb-10">
                 <svg
                   version="1.1"
                   viewBox="0 0 3368 512"
@@ -48,7 +50,7 @@ export default function Footer() {
                     </g>
                   </g>
                 </svg>
-              </a>
+              </Link>
               <p className="text-justify">
                 Tailwindow is a collection of UI Components created using
                 Tailwind CSS Framework. The UI Components gives you all of the
@@ -65,24 +67,24 @@ export default function Footer() {
               </div>
               <ul className="leading-8">
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     About Us
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     Terms &amp; Conditions
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     Privacy Policy
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     Contact Us
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -94,35 +96,35 @@ export default function Footer() {
               </div>
               <ul className="leading-8">
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     Getting Started With HTML and CSS
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     What Is Flex And When to Use It?
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     How TailwindCSS Can Help Your Productivity?
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     5 Tips to Make Responsive Website
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-blue-400">
+                  <Link to="#" className="hover:text-blue-400">
                     See More
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
             <div className="px-4 my-4 w-full sm:w-auto xl:w-1/5">
               <div>
-                <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">
+                <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-[--azul-medio]">
                   Connect With Us
                 </h2>
               </div>
@@ -190,7 +192,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="bg-indigo-700 py-4 text-gray-100">
+      <div className="bg-[--amarelo] py-4 text-gray-100">
         <div className="container mx-auto px-4">
           <div className="-mx-4 flex flex-wrap justify-between">
             <div className="px-4 w-full text-center sm:w-auto sm:text-left">
@@ -202,7 +204,7 @@ export default function Footer() {
               - 2022 Tailwindow. All Rights Reserved.
             </div>
             <div className="px-4 w-full text-center sm:w-auto sm:text-left">
-              Made with ❤️ by Tailwindow.
+              Made with ❤️ by EduNova.
             </div>
           </div>
         </div>
