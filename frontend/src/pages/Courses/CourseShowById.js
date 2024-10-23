298904d64db8936b11e181b1b012e91607cb7dcc
Clara Quintela <claraquintela@gmail.com>
2024-10-23 11:04:24 -0400

    Change Docker configurations

--- frontend/src/pages/Courses/CourseShowById.js
+++ frontend/src/pages/Courses/CourseShowById.js
@@ -1,3 +1,13 @@
+import { useParams } from "react-router-dom";
+
 export default function CourseShowById({ t }) {
-  return <div>Hello</div>;
+  const parametro = useParams();
+  console.log("parametro ", parametro);
+
+  return (
+    <div>
+      <h1>Hello Curso {parametro.id} teste</h1>
+      <p>hdgajhdafedsffffffd</p>
+    </div>
+  );
 }
