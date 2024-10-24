9e38b3b4fbbda40bf9710f16a84d89884eca6802
Clara Quintela <claraquintela@gmail.com>
2024-10-24 16:36:12 -0400

    Translate pages sign up and sign in - done

--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -75,9 +75,7 @@ export default function Signin({ t }) {
             <img src={logo} className="w-48 mx-auto" alt="EduNova Logo" />
           </div>
           <div className="mt-12 flex flex-col items-center">
-            <h1 className="text-2xl xl:text-3xl font-extrabold">
-              {t("sign_in")}
-            </h1>
+            <h1 className="text-2xl xl:text-3xl font-extrabold">Login</h1>
             <form onSubmit={handleSubmit} className="w-full flex-1 mt-8">
               <div className="mx-auto max-w-xs">
                 {error && (
@@ -97,7 +95,7 @@ export default function Signin({ t }) {
                 <input
                   className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                   type="password"
-                  placeholder={t("password")}
+                  placeholder={t("signup_password")}
                   name="password"
                   value={formData.password}
                   onChange={handleChange}
